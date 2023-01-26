a621979634aa37d0d36c47eca386cdb2d76c2176
TangiLC <117976823+TangiLC@users.noreply.github.com>
2023-01-26 21:04:20 +0100

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,33 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+const closeBtn = document.querySelector(".close");    //declaration close button
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal event on click X
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+   modalbg.style.display = "none";
+}
+
+
+
